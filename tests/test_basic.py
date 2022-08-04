afbcb0035eeeca6818ce7f4c581619a48b10c2e8
Nikolai <nikolai.karasoff@yandex.ru>
2022-08-04 20:46:34 +0300

    Update test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,12 @@
-import cmake_example as m
+from edit_distance_tools import weighted_levenshtein
 
 
 def test_main():
-    assert m.__version__ == "0.0.1"
-    assert m.add(1, 2) == 3
-    assert m.subtract(1, 2) == -1
+    s1 = [1, 5, 3]
+    s2 = [1, 2, 3, 4]
+
+    w1 = [0.1, 0.2, 0.3]
+    w2 = [0.1, 0.25, 0.4, 1.0]
+
+    x = weighted_levenshtein(s1, s2, w1, w2, 3, 4)
+    assert x == 1.4500000476837158
